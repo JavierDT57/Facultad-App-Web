d3541db9ac9d66fefb27d861528efb21963b1de7
Juan Manuel Rodriguez Sotarriba <rodgz.stria@gmail.com>
2024-10-28 19:39:59 -0600

    Add Personal.php

---
+++ personal.php
@@ -0,0 +1,203 @@
+<?php
+$xml = simplexml_load_file("xmlgeneral.xml");
+$puestos = $xml->xpath("/facultad/administracion/puestos/puesto");
+
+if (isset($_GET["id"])) {
+  # Recuperar datos del ID dado
+  $administrativo = $xml->xpath("/facultad/administracion/personal/empleados/empleado[@id_empleado=" . $_GET["id"] . "]");
+  $puestos_asignados = array();
+  foreach ($administrativo[0]->puestos_imp->puesto as $puesto) {
+    array_push($puestos_asignados, strval($puesto->attributes()));
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Personal Administrativo</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/jquery-ui.min.css"/>
+    <link rel="stylesheet" href="css/estilos.css"/>
+    <script src="js/external/jquery/jquery.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"/>
+</head>
+<script type="text/javascript">
+function guardar() {
+    $.ajax({
+        // Guardar/Editar Registro
+        url: "include/funciones.php",
+        type: "post",
+        data: $("#formulario").serialize(),
+        success: function (response) {
+            if (response == "0") {
+                // Error: ID de empleado ya existe
+                $("<div>El ID ya ha sido establecido en otro empleado.</div>").dialog({
+                    title: "Error",
+                    resizable: false,
+                    height: "auto",
+                    width: 400,
+                    modal: true,
+                    buttons: {
+                        "Entendido": function() {
+                            $(this).dialog("close");
+                        }
+                    }
+                });
+            } else {
+                $("<div>Acción Completada.</div>").dialog({
+                    title: "Acción Completada",
+                    resizable: false,
+                    height: "auto",
+                    width: 400,
+                    modal: true,
+                    buttons: {
+                        "Entendido": function() {
+                            $(this).dialog("close");
+                            document.location = 'xmlpersonal.xml';
+                        }
+                    }
+                });
+            }
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+        }
+    });
+}
+</script>
+<body class="fondo_main">
+<br><br>
+<h2 align="center" class="titulo">Registrar Personal Administrativo</h2>
+<form role="form" id="formulario" name="formulario" action="javascript:guardar();">
+    <div class="container">
+        <br>
+        <div id="accordion">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                    <h5 class="mb-0">
+                        <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Datos Personales
+                        </button>
+                    </h5>
+                </div>
+
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div class="card-body">
+                        <div class="container">
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label>ID Empleado:</label>
+                                    <input type="number" name="id_empleado" class="form-control" required
+                                    <?php if (isset($_GET["id"])) { echo "value='" . $_GET["id"] . "' disabled"; } ?>>
+                                    <input type="hidden" name="id" id="id" value="<?php echo (isset($_GET["id"]) ? $_GET["id"] : "") ?>" />
+                                    <input type="hidden" name="acc" id="acc" value="<?php echo (isset($_GET["id"]) ? "2" : "1") ?>" />
+                                    <input type="hidden" name="tipo" id="tipo" value="1" />
+                                </div>
+                                <div class="form-group col-md-8">
+                                    <label>Nombre completo:</label>
+                                    <input type="text" name="nombre" class="form-control" required
+                                    <?php if (isset($_GET["id"])) { echo "value='" . $administrativo[0]->nombre . "'"; } ?>>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label>Departamento:</label>
+                                    <input type="text" name="departamento" class="form-control" required
+                                    <?php if (isset($_GET["id"])) { echo "value='" . $administrativo[0]->departamento . "'"; } ?>>
+                                </div>
+                                <div class="form-group col-md-8">
+                                    <label>Correo:</label>
+                                    <input type="text" name="correo" class="form-control" required
+                                    <?php if (isset($_GET["id"])) { echo "value='" . $administrativo[0]->correo . "'"; } ?>>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header" id="headingTwo">
+                    <h5 class="mb-0">
+                        <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Puestos
+                        </button>
+                        <button type="button" class="btn btn-success" id="clonar_puesto">Agregar Puesto</button>
+                        <button type="button" class="btn btn-danger" id="eliminar_puesto">Eliminar Puesto</button>
+                    </h5>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                    <div class="card-body" id="padre">
+                        <div id="puestos"><?php
+                            if (isset($puestos_asignados) && (count($puestos_asignados) > 0)) {
+                                for ($i = 0; $i < count($puestos_asignados); $i++) {
+                                    echo "<div class='form-row col-lg-12' id='puesto" . $i . "'>";
+                                    echo "<div class='from-group col-lg-8'>
+                                        <select class='custom-select col-lg-12' name='puestos[]'>";
+                                    foreach ($puestos as $puesto) {
+                                        $text = "";
+                                        if (strval($puesto->clave_puesto) == $puestos_asignados[$i]) {
+                                            $text = "Selected";
+                                        }
+                                        echo "<option $text value='" . $puesto->clave_puesto . "'> " . $puesto->nombre . "</option>";
+                                    }
+                                    echo "</select>
+                                        </div>
+                                    </div>";
+                                }
+                            } else {
+                                echo "<div class='form-row col-lg-12' id='puesto0'>";
+                                echo "<div class='from-group col-lg-8'>
+                                    <select class='custom-select col-lg-12' name='puestos[]'>";
+                                foreach ($puestos as $puesto) {
+                                    echo "<option value='" . $puesto->clave_puesto . "'> " . $puesto->nombre . "</option>";
+                                }
+                                echo "</select>
+                                    </div>
+                                </div>";
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <?php if (!isset($_GET["id"])): ?>
+            <div align="center" style="margin-bottom: 20px;">
+                <button type="submit" class="btn btn-primary boton_guardar" style="width:350px;">Guardar</button>
+            </div>
+        <?php else: ?>
+            <div align="center" style="margin-bottom: 20px;">
+                <button type="submit" class="btn btn-primary boton_editar" style="width:350px;">Editar</button>
+            </div>
+        <?php endif; ?>
+    </div>
+</form>
+</body>
+<script>
+$("#clonar_puesto").on("click", function() {
+    var cantidad = document.getElementsByName("puestos[]").length;
+    while ($("#puesto" + cantidad).length == 1) {
+        cantidad = cantidad + 1;
+    }
+    $("#puestos").append("<div class='form-row col-lg-12' id='puesto" + cantidad + "'>" + document.getElementById("puesto0").innerHTML + "</div>")
+});
+
+$("#eliminar_puesto").on("click", function() {
+    var cantidad = document.getElementsByName("puestos[]").length;
+    console.log(cantidad);
+    if (cantidad != 1) {
+        $("#puesto" + (cantidad - 1)).remove();
+    } else {
+        alert("No se puede eliminar el último puesto");
+    }
+});
+</script>
+</html>
